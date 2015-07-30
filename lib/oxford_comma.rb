c37c9733d2539405045d7af1c32a12e1a7aa2df0
nikila-saravanan <nikila.saravanan@gmail.com>
2015-07-30 16:08:40 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[0]
+  elsif array.length == 2
+    "#{array[0]} and #{array[1]}"
+  else
+    x = 0
+    target_string = ""
+    while x < (array.length - 1)
+      target_string += "#{array[x]}, "
+      x += 1
+    end
+    target_string + "and #{array[-1]}"
+  end
 end
